7e38eaf663c78289dc802a9d5d96f6dc5013ca86
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-06-05 00:58:04 -0300

    estilo css completo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
 const cipher = {
   // ...
 };
+function cipher() {
+      
+}
+// //A = 65
+// //B = 67
+// //'A'.charCodeAt(0) // 65
+
+
+
+
+
+// // function cifra(string) {
+
+// // }
+
+// // (codigoDaLetra + desloc) % tamDoAlfabeto
+// // que seria: (codigoDaLetra + 3) % 26[quantidade de letras alfabeto]
+
+// // ___________
+// // ((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById('criptografar').addEventListener('click',cipher)
+document.getElementById('traduzir').addEventListener('click',traduzir)
+
+function verificar() {
+    let inputNumero = document.getElementById('entradanumber')
+    let entradaNumero = Number(inputNumero.value) // quantidas de vezes que vai pular number
+    let res = document.getElementById('res')
+
+    let inputTexto = document.getElementById('entradatext') // frase que o cliente colocou
+    let entradaTexto = String(inputTexto.value).toUpperCase() //frase texto string //larissa
+    res.innerHTML = `(${entradaNumero} || ${entradaTexto})`
+}
