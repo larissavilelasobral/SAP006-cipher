15d368d4f051d73c68699b0c905ec3fa6147ff60
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-06-09 16:56:52 -0300

    codigo encode e alteração do leyout

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,18 @@
-const cipher = {
-  // ...
-};
+const characters = 32;
+const maxCharacters = 237;
 
+const cipher = {
+  encode: function(entradanumber,entradatext) {
+    let message = "";
+
+    for (let i = 0; i < entradatext.length; i++) {
+      let code = entradatext.charCodeAt(i);
+      if(code >= characters && code <= maxCharacters){
+        message += String.fromCharCode((code - characters + entradanumber) %205 + characters);}
+      else { code += entradatext.charAt(i);
+      }
+    }
+    return message;
+  }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const buttonCrip = document.getElementById('criptografar');
+//transformando o primeiro botão em uma variavel
+
+    function criptografarCode(e) { //função do primeiro botão
+        e.preventDefault(); //depois que o click acontece o evento para
+        const entradanumber = Number(document.getElementById("entradanumber").value);
+        let entradatext = document.getElementById("entradatext").value;
+        const cipherCode = cipher.encode(entradanumber,entradatext);
+            document.getElementById("saidatext").value = cipherCode;
+}
+buttonCrip.addEventListener("click", criptografarCode);
