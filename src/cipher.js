248c1ed2e458ef58bd6c356b6e6e475ccec53306
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-06-10 21:29:16 -0300

    readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,41 @@
-const characters = 32; //?
-const maxCharacters = 237; //?
 var quantidadeLetras = 65;
 
 const cipher = { //variavel cipher em objeto
   encode: function(entradanumber,entradatext) { //funcão encode e seus parametros
     let message = ""; //
-
+    if (entradanumber == "" || entradatext == ""){
+      throw new TypeError('Valor string não adicionado. index.js:10', 'index.js', 10)
+    }
     for (let i = 0; i < entradatext.length; i++) { //loop que rota até o tamanho do texto de entrada
       let codeEncode = entradatext.charCodeAt(i); //entra a palavra, e o numero zero; primeira letra e sai o numero dela C=67
-      if (codeEncode >= characters && codeEncode<=maxCharacters){
-      //passando as letras criptografadas para a variavel message atreves de um incremento
-      message += String.fromCharCode((codeEncode - quantidadeLetras + entradanumber)%26+quantidadeLetras);
-      }else{
-      codeEncode += entradatext.charAt(i); //??
+      if(codeEncode >= 32 && codeEncode <= 64){
+        message += entradatext.charAt(i);
+      }else if(codeEncode >= 65 && codeEncode<=90){
+        message += String.fromCharCode((codeEncode - quantidadeLetras + entradanumber)%26+quantidadeLetras);
+      }else if(codeEncode >= 97 && codeEncode <= 122){
+        message += String.fromCharCode((codeEncode - 26 + entradanumber)%26 + 65)
       }
-
-      // if(code >= characters && code <= maxCharacters){//if condicao
-      //   message += String.fromCharCode((code - characters + entradanumber) %205 + characters);
-
-      // } else { 
-      //     code += entradatext.charAt(i);
-      //   //transformando numero + offset em letra criptada. 
-      // }
     }
     return message;
   },
 decode: function(entradanumber,entradatext){
+  if (entradanumber == "" || entradatext == ""){
+    throw new TypeError('Valor string não adicionado. index.js:10', 'index.js', 10)
+  }
   let messageSegunda = "";
     for (let i = 0; i < entradatext.length; i++) {
-      let inversa = ((entradatext.charCodeAt(i) - entradanumber -quantidadeLetras)%26 + quantidadeLetras)
-      messageSegunda += (String.fromCharCode(inversa))
+      let codeDecode = entradatext.charCodeAt(i);
+      
+      let decodeFinal =  ((codeDecode - entradanumber - 65) %26 + 65)
+      if(codeDecode >= 32 && codeDecode <= 64){
+        messageSegunda += entradatext.charAt(i);
+      }else if(codeDecode >= 65 && codeDecode<=90){
+        messageSegunda += (String.fromCharCode(decodeFinal))
+      }else if(codeDecode >= 97 && codeDecode <= 122){
+        messageSegunda += String.fromCharCode((codeDecode - 97 - entradanumber)%26 + 97)
+      }
    }
 return messageSegunda;
-
-  //   for (let i = 0; i < entradatext.length; i++) {
-  //     let codeDecode = entradatext.charCodeAt(i);
-  //     if (codeDecode >= characters && codeDecode<=maxCharacters){
-  //     messageSegunda += String.fromCharCode((codeDecode - characters - entradanumber)%205 + characters);
-  //   }else{
-  //   codeDecode += entradatext.charAt(i);
-  //  }
-  //  return messageSegunda;
-  // }
 }
 }
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import cipher from './cipher.js'; //importando a variavel cipher da pasta cipher
 const buttonCriptografar = document.getElementById("criptografar");
 //transformando o primeiro botão em uma variavel
 
-    function criptografarCode(e) { //função do primeiro botão
-        e.preventDefault(); //depois que o click acontece o evento para.
+    function criptografarCode() { //função do primeiro botão
         const entradanumber = Number(document.getElementById("entradanumber").value);//transformando a entrada value em numero e em variavel.
         const entradatext = document.getElementById("entradatext").value; //pegando o value da entrada e transformando em variavel
+        // if (entradanumber == "" || entradanumber == "null"){
+        //     throw new TypeError('Valor string não adicionado. index.js:10', 'index.js', 10)
+        // }else if( entradatext == "" || entradatext == "null"){
+        //     throw new TypeError('Valor number não adicionado. index.js:9','index.js', 9)
+        // }
         const cipherCode = cipher.encode(entradanumber,entradatext); //chamando a variavel cipher e dentro dela a função encode com os paremetros.
             document.getElementById("entradatext").value = cipherCode; //colocando o valor de cipherCode como value do textarea entradatext.
 }
@@ -16,8 +20,7 @@ buttonCriptografar.addEventListener("click", criptografarCode); //adicionando o 
 
 const buttonTraduzir = document.getElementById("traduzir");
 
-function traduzir(e) {
-    e.preventDefault();
+function traduzir() {
     const entradanumber = Number(document.getElementById("entradanumber").value);
     const entradatext = document.getElementById("entradatext").value;
     const cipherCode = cipher.decode(entradanumber,entradatext);
