bff21af255cb2a6851b071335f85e6acfc325172
Larissa Vilela Sobral <larissavilelasobral@gmail.com>
2021-10-19 23:15:00 -0300

    readme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import cipher from './cipher.js'; 
 
-//console.log(cipher);
-//toUpperCase()
-
 const buttonCriptografar = document.getElementById("criptografar");
 
 function criptografarCode() { 
     const entradanumber = Number(document.getElementById("entradanumber").value);
     const entradatext = document.getElementById("entradatext").value; 
-     const cipherCode = cipher.encode(entradanumber,entradatext); 
-        document.getElementById("entradatext").value = cipherCode; 
+    const cipherCode = cipher.encode(entradanumber,entradatext); 
+    document.getElementById("entradatext").value = cipherCode; 
 }
+
 buttonCriptografar.addEventListener("click", criptografarCode); 
 
 const buttonTraduzir = document.getElementById("traduzir");
